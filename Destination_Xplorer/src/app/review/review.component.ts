a319313ed65220830d60f9fc6857a0e5d55208e9
Bhoomichha <bdangol@my.centennialcollege.ca>
2023-08-01 01:23:02 -0400

    Adding the review component and css

--- Destination_Xplorer/src/app/review/review.component.ts
+++ Destination_Xplorer/src/app/review/review.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./review.component.scss']
 })
 export class ReviewComponent {
+  reviewTitle: string = '';
+  reviewDescription: string = '';
+  uploadedPictures: string[] = [];
 
+  onFileSelected(event: any) {
+    // Handle file selection
+    const selectedFiles = event.target.files;
+    if (selectedFiles) {
+      // Convert FileList to an array and process each selected file
+      Array.from(selectedFiles).forEach((file: unknown) => {
+        // Explicitly cast the file to Blob type
+        const blobFile = file as Blob;
+
+        // Convert the file to a data URL to display the image
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.uploadedPictures.push(e.target.result);
+        };
+        reader.readAsDataURL(blobFile);
+      });
+    }
+  }
+
+  submitReview() {
+    // Here, you can handle the submission logic and save the review, including the pictures, to your backend or storage service.
+    // You can access the reviewTitle, reviewDescription, and uploadedPictures arrays to process the data.
+    // For simplicity, we won't implement the actual submission logic here.
+    console.log('Review Title:', this.reviewTitle);
+    console.log('Review Description:', this.reviewDescription);
+    console.log('Uploaded Pictures:', this.uploadedPictures);
+  }
 }
